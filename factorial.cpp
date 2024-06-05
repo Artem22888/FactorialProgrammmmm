c25114cc169fc066da0d83e33d06b5ced9be9a32
Artem22888 <delovayacolbaska@gmail.com>
2024-06-05 21:53:13 +0300

    Добавлена функция вычисления факториала с помощью рекурсии

--- factorial.cpp
+++ factorial.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
-#include <stdexcept>
-
-// Функция для вычисления факториала
-unsigned long long factorial(int n) {
-    if (n < 0) {
-        throw std::invalid_argument("Factorial is not defined for negative numbers");
-    }
-    unsigned long long result = 1;
-    for (int i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
-}
 
 int main() {
-    int number;
-    std::cout << "Enter a number: ";
-    std::cin >> number;
-
-    try {
-        unsigned long long fact = factorial(number);
-        std::cout << "Factorial of " << number << " is " << fact << std::endl;
-    } catch (const std::invalid_argument& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-
+    int num1, num2;
+    
+    // Запрос ввода двух чисел
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    
+    // Вычисление и вывод суммы
+    int sum = num1 + num2;
+    std::cout << "Сумма чисел: " << sum << std::endl;
+    
+    // Вычисление и вывод произведения
+    int product = num1 * num2;
+    std::cout << "Произведение чисел: " << product << std::endl;
+    
     return 0;
 }
